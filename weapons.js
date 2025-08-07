003ed6ab4fb004fe443591a92bcb1521d700e94e
PS164435 <100274100+PS164435@users.noreply.github.com>
2025-08-07 15:12:04 +0200

    Update weapons.js

--- weapons.js
+++ weapons.js
@@ -1,7 +1,7 @@
-const primaryWeapons = {
+const options = {
   "======== ASSAULT RIFLE ========": {
     " -- DEFAULT WEAPON -- ": [
-      { name: '416-C CARBINE', value: 'Weapons/416-C CARBINE.png' },
+      { name: '416-C CARBINEE', value: 'Weapons/416-C CARBINE.png' },
       { name: '552 COMMANDO', value: 'Weapons/552 COMMANDO.png' },
       { name: '556XI', value: 'Weapons/556XI.png' },
       { name: 'AK-12', value: 'Weapons/AK-12.png' },
@@ -31,6 +31,7 @@ const primaryWeapons = {
       { name: 'V308', value: 'Weapons/V308.png' },
     ],
     " -- CUSTOM WEAPON -- ": [
+      
     ],
   },
   "======== LIGHT MACHINE GUN ========": {
@@ -43,7 +44,8 @@ const primaryWeapons = {
       { name: 'MLG-E', value: 'Weapons/MLG-E.png' },
       { name: 'T-95 LSW', value: 'Weapons/T-95 LSW.png' },
     ],
-    " -- CUSTOM WEAPON -- ": [ 
+    " -- CUSTOM WEAPON -- ": [
+      
     ],
   },
   "======== MARKSMAN RIFLE ========": {
@@ -56,13 +58,16 @@ const primaryWeapons = {
       { name: 'SR-25', value: 'Weapons/SR-25.png' },
     ],
     " -- CUSTOM WEAPON -- ": [
+      
     ],
   },
   "======== SNIPER RIFLE ========": {
     " -- DEFAULT WEAPON -- ": [
       { name: 'CRSX 300', value: 'Weapons/CRSX 300.png' },
+      
     ],
     " -- CUSTOM WEAPON -- ": [
+      
     ],
   },
   "======== SUBMACHINE GUN ========": {
@@ -86,8 +91,10 @@ const primaryWeapons = {
       { name: 'UMP45', value: 'Weapons/UMP45.png' },
       { name: 'UZK50GI', value: 'Weapons/UZK50GI.png' },
       { name: 'VECTOR .45 ACP', value: 'Weapons/VECTOR .45 ACP.png' },
+      
     ],
     " -- CUSTOM WEAPON -- ": [
+      
     ],
   },
   "======== SLUG SHOTGUN ========": {
@@ -97,12 +104,14 @@ const primaryWeapons = {
       { name: 'TCSG12', value: 'Weapons/TCSG12.png' },
     ],
     " -- CUSTOM WEAPON -- ": [
+      
     ],
   },
   "======== SHOTGUN ========": {
     " -- DEFAULT WEAPON -- ": [
       { name: 'FO-12', value: 'Weapons/FO-12.png' },
       { name: 'ITA12L', value: 'Weapons/ITA12L.png' },
+      { name: 'ITA12S', value: 'Weapons/ITA12S.png' },
       { name: 'M1014', value: 'Weapons/M1014.png' },
       { name: 'M590A1', value: 'Weapons/M590A1.png' },
       { name: 'M870', value: 'Weapons/M870.png' },
@@ -113,27 +122,12 @@ const primaryWeapons = {
       { name: 'SPAS-12', value: 'Weapons/SPAS-12.png' },
       { name: 'SPAS-15', value: 'Weapons/SPAS-15.png' },
       { name: 'SUPER 90', value: 'Weapons/SUPER 90.png' },
-      { name: 'SUPERNOVA', value: 'Weapons/SUPERNOVA.png' },
-    ],
-    " -- CUSTOM WEAPON -- ": [ 
-    ],
-  },
-  "======== SHIELD ========": {
-    " -- DEFAULT WEAPON -- ": [
-      { name: 'BALLISTIC SHIELD', value: 'Weapons/BALLISTIC SHIELD.png' },
-    ],
-    " -- CUSTOM WEAPON -- ": [
-    ],
-  },
-};
-
-const secondaryWeapons = {
-  "======== SHOTGUN ========": {
-    " -- DEFAULT WEAPON -- ": [
-      { name: 'ITA12S', value: 'Weapons/ITA12S.png' },
       { name: 'SUPER SHORTY', value: 'Weapons/SUPER SHORTY.png' },
+      { name: 'SUPERNOVA', value: 'Weapons/SUPERNOVA.png' },
+      
     ],
     " -- CUSTOM WEAPON -- ": [
+      
     ],
   },
   "======== MACHINE PISTOL ========": {
@@ -146,6 +140,7 @@ const secondaryWeapons = {
       { name: 'SPSMG9', value: 'Weapons/SPSMG9.png' },
     ],
     " -- CUSTOM WEAPON -- ": [
+      
     ],
   },
   "======== REVOLVER ========": {
@@ -153,8 +148,10 @@ const secondaryWeapons = {
       { name: 'KERATOS .357', value: 'Weapons/KERATOS .357.png' },
       { name: 'LFP586', value: 'Weapons/LFP586.png' },
       { name: 'BAILIFF 410', value: 'Weapons/BAILIFF 410.png' },
+      
     ],
     " -- CUSTOM WEAPON -- ": [
+      
     ],
   },
   "======== HANDGUN ========": {
@@ -179,13 +176,25 @@ const secondaryWeapons = {
       { name: 'USP40', value: 'Weapons/USP40.png' },
     ],
     " -- CUSTOM WEAPON -- ": [
+      
+    ],
+  },
+  "======== SHIELD ========": {
+    " -- DEFAULT WEAPON -- ": [
+      { name: 'BALLISTIC SHIELD', value: 'Weapons/BALLISTIC SHIELD.png' },
+      
+    ],
+    " -- CUSTOM WEAPON -- ": [
+      
     ],
   },
   "======== HAND CANNON ========": {
     " -- DEFAULT WEAPON -- ": [
       { name: 'GONNE-6', value: 'Weapons/GONNE-6.png' },
+      
     ],
-    " -- CUSTOM WEAPON -- ": [ 
+    " -- CUSTOM WEAPON -- ": [
+      
     ],
   },
 };
